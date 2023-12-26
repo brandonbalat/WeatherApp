16cfd1e052bc038023a08656c461778d69ce442a
Brandon Balat <brandonbalat@gmail.com>
2023-12-25 23:23:34 -0800

    New weather app

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,32 @@
-const App = () => {
+
+import Search from "./components/Search"
+import Forecast from "./components/Forecast"
+import useForecast from "./hooks/useForecast"
+
+const App = (): JSX.Element => {
+  //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+  const {
+    term,
+    options,
+    forecast,
+    onInputChange,
+    onOptionSelect,
+    onSubmit
+  } = useForecast()
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
